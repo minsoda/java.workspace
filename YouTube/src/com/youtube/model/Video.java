c3fc472a6dfa9d9b96989ecfff32b5b6d36d3cc2
minsodadadadadada <5sunhee6@naver.com>
2023-05-23 15:22:38 +0900

    .

--- YouTube/src/com/youtube/model/Video.java
+++ YouTube/src/com/youtube/model/Video.java
@@ -1,5 +1,49 @@
 package com.youtube.model;
 
+import java.util.Date;
+
 public class Video {
 
+	public String title;
+	public String comment;
+	public Date uploadAt;
+	public int count; // views
+	public String imUrl;
+	public String fileUrl;
+	
+	/*
+	 * Create : 추가
+	 * Read : 읽기 (1개, 목록)
+	 * Update : 수정
+	 * Delete : 삭제
+	 * 
+	 * */
+	
+	//영상 업로드 
+	public void upload() {
+		
+		
+	}
+	
+	// 영상 목록
+	public void viewList() {
+	
+	}
+	
+	
+	//영상 1개 보기
+	public void viewVideo() {
+	
+	}
+	
+	//동영상 수정
+	public void updateVideo() {
+	
+	}
+	
+	//동영상 삭제
+	public void deleteViedeo() {
+		
+	}
+	
 }
